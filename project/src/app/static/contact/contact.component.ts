2152336572909d2a851a845aaaf123d51763b66e
Deyan Yosifov <deqnpavlikov@gmail.com>
2024-03-18 22:24:49 +0200

    adding static contacts page with font-awesome icons

--- project/src/app/static/contact/contact.component.ts
+++ project/src/app/static/contact/contact.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  locationUrl = "https://www.google.com/maps/place/42%C2%B042'03.2%22N+23%C2%B022'23.8%22E/@42.7013827,23.3547691,15z/data=!4m13!1m8!3m7!1s0x40a8fec1c85bf089:0xa01269bf4c10!2z0JHRitC70LPQsNGA0LjRjw!3b1!8m2!3d42.733883!4d25.48583!16zL20vMDE1cWg!3m3!8m2!3d42.700894!4d23.373271?hl=bg&entry=ttu"
 
 }
