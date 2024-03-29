7beb431a34b6e747aee80551487ab0bff19098d9
Deyan Yosifov <deqnpavlikov@gmail.com>
2024-03-29 04:03:53 +0200

    removing about page

--- project/src/app/static/static-routing.module.ts
+++ project/src/app/static/static-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'about', component: AboutComponent },
   { path: 'contacts', component: ContactComponent },
 ];
 @NgModule({
--- project/src/app/static/static.module.ts
+++ project/src/app/static/static.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule } from '@angular/router';
 import { StaticRouterRoutingModule } from './static-routing.module';
@@ -9,7 +8,6 @@ import { StaticRouterRoutingModule } from './static-routing.module';
 
 @NgModule({
   declarations: [
-    AboutComponent,
     ContactComponent
   ],
   imports: [
