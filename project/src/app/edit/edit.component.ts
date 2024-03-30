d28d03e91dbe827238361fe260ba5fcfe101b664
Deyan Yosifov <deqnpavlikov@gmail.com>
2024-03-30 17:35:57 +0200

    adding some logic for edit page

--- project/src/app/edit/edit.component.ts
+++ project/src/app/edit/edit.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Art } from '../types/art';
+import { ApiService } from '../api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
+  posts = {} as Art;
 
+  constructor(private apiService: ApiService, private activeRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe((data) => {
+      const id = data['postId'];
+
+      this.apiService.getOneArt(id).subscribe((artPost) => {
+        this.posts = artPost;
+      });
+    })
+  }
 }
