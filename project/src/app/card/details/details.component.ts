e5a73f8170b25db1bd4069122e876122ec4f47c6
Deyan9469 <deqnpavlikov@gmail.com>
2024-04-02 22:41:33 +0300

    adding view to buttons if not the owner

--- project/src/app/card/details/details.component.ts
+++ project/src/app/card/details/details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { Art } from 'src/app/types/art';
+import { User } from 'src/app/types/user';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-details',
@@ -10,8 +12,17 @@ import { Art } from 'src/app/types/art';
 })
 export class DetailsComponent implements OnInit {
   post = {} as Art;
+  user?: User | null;
 
-  constructor(private apiService: ApiService, private activeRoute: ActivatedRoute, private router: Router){}
+  constructor(private apiService: ApiService, private activeRoute: ActivatedRoute, private router: Router, private userService: UserService) {
+    const user = this.userService.user$.subscribe(x => this.user = x)
+
+  }
+
+  get isLoggedIn(): boolean {
+    return !!this.user
+  }
+
 
   ngOnInit(): void {
     this.activeRoute.params.subscribe((data) => {
@@ -19,14 +30,21 @@ export class DetailsComponent implements OnInit {
 
       this.apiService.getOneArt(id).subscribe((post) => {
         this.post = post;
+
       });
     });
   }
 
-  onDelete(_id:string): void{
-    this.apiService.onDelete(_id).subscribe(()=>{
+  onDelete(_id: string): void {
+    this.apiService.onDelete(_id).subscribe(() => {
       this.router.navigate(['/']);
     });
   }
+
+  isAuth(post: Art) {
+    const isAuth: boolean = post._ownerId === this.user?._id
+    return isAuth
+  }
+
 
 }
